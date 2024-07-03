5288754e7f86026100c15b3e6745cb77741a8dd6
Shivam Tiwari <shivamtiwari@Shivams-MacBook-Air.local>
2024-07-03 10:46:36 +0530

    Fix: Committed changes in index.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+const express = require('express');
+const app = express();
+app.use(express.json()); // middleware
+
 let courses = [
-    {id : 1, name : "java"},
-    {id : 2, name : "javascript"},
-    {id : 3, name : "python"}
+    { id: 1, name: "java" },
+    { id: 2, name: "javascript" },
+    { id: 3, name: "python" }
 ];
 
-app.get('/courses', (req, res) => {
-    rep.json(courses);
+app.get('/', (req, res) => {
+    res.send("Crazy!");
 });
 
-app.listen(3000, () => {
-    console.log("server started")
+app.get('/courses', (req, res) => {
+    res.json(courses);
+});
+
+app.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name,
+    };
+    courses.push(course);
+    res.status(201).json(courses);
+    
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
